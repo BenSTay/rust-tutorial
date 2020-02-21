d162f8aaab882f3a08820c3d75dbaf99feb68faf
Benjamin Taylor <staylor.ben@gmail.com>
2020-02-20 21:25:10 -0800

    Program now compares guess to random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number between 1 and 10!\nPlease input your guess.");
@@ -12,6 +13,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u8 = guess.trim().parse()
+        .expect("Not a number!");
+
     println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number)
+    println!("The secret number is {}", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("You got it!"),
+    }
 }
